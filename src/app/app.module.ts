94718af1f729d7bf3b620bea694999b588711fde
pan0003 <75246763+pan0003@users.noreply.github.com>
2023-12-02 16:14:57 -0600

    Created html Play game page (in progress)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayGameComponent } from './play-game/play-game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayGameComponent
   ],
   imports: [
     BrowserModule,
