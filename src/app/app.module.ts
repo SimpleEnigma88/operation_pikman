a8a801c9c3e627109f8d83a9b0800caa350e866b
sdlvlamp3 <140354850+sdlvlamp3@users.noreply.github.com>
2023-12-04 18:57:41 -0600

    nav, create, edit basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabQComponent } from './question-form/mat-tab-q/mat-tab-q.component';
+import { CreateQuestionFormComponent } from './question-form/create-question-form/create-question-form.component';
+import { EditQuestionsComponent } from './question-form/edit-questions/edit-questions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatTabQComponent,
+    QuestionFormComponent,
+    CreateQuestionFormComponent,
+    EditQuestionsComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/question-form/question-form.component.ts
+++ src/app/question-form/question-form.component.ts
@@ -1,9 +1,15 @@
-import { Component, OnInit, } from '@angular/core';
-
+import { Component, } from '@angular/core';
+import { MatTabQComponent } from './mat-tab-q/mat-tab-q.component';
+import { CreateQuestionFormComponent } from './create-question-form/create-question-form.component';
 @Component({
   selector: 'app-question-form',
   templateUrl: './question-form.component.html',
-  styleUrls: ['./question-form.component.css']
+  styleUrls: ['./question-form.component.css'],
+  standalone: true,
+  imports: [
+    MatTabQComponent,
+    CreateQuestionFormComponent,
+  ]
 })
 export class QuestionFormComponent {
 
