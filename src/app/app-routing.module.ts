7b34d654bcf570acb865a85aa74329604cc59607
Madison S <31750412+mhizer8@users.noreply.github.com>
2023-12-07 18:22:54 -0600

    Created login/signup component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AuthComponent } from './auth/auth.component';
+import { PlayGameComponent } from './play-game/play-game.component';
 
 const routes: Routes = [
-  {path: 'landing', component: LandingPageComponent},
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: 'landing', component: LandingPageComponent },
+  { path: 'auth', component: AuthComponent},
+  { path: 'play', component: PlayGameComponent}
 ];
 
 @NgModule({
