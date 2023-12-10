f436c9dbf4ab1635b76c6d143c7bde83c6b46ddc
Robert Andrew Moore <ra.moore573@gmail.com>
2023-12-10 09:21:01 -0600

    Remove authService dependency in TriviaService constructor

--- src/app/trivia.service.ts
+++ src/app/trivia.service.ts
@@ -15,7 +15,6 @@ export class TriviaService {
   questionList: any[] = [];
 
   constructor(
-    private authService: AuthService,
     private http: HttpClient,
     private snackBar: MatSnackBar
   ) { }
