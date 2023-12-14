df82d39583d988d0fe042d176e9eccee330e35df
Robert Andrew Moore <ra.moore573@gmail.com>
2023-12-13 21:14:37 -0600

    Added dynamic Q# and Movie posters

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -11,20 +11,45 @@ import { QuestionResultService } from '../result.service';
 })
 export class QuestionComponent implements OnInit, OnDestroy {
   questionList: any[] = [];
-  currentQuestion: string = "test";
-  correctAnswer: string = "true";
+  currentQuestion: string = "";
+  correctAnswer: string = "";
+  currentMovie: string = "";
   subs: Subscription;
   counter: number = 0;
+  Tsub: Subscription;
+  Qsub: Subscription;
+  Msub: Subscription;
+  result: string = "";
+  showNext: boolean = false;
+  picUrl: string = "";
 
   constructor(private triviaService: TriviaService, private questionResult: QuestionResultService) { }
 
   goToNextQuestion() {
     this.counter++;
     this.currentQuestion = this.questionList[this.counter].question;
+    console.log(this.currentQuestion);
     this.correctAnswer = this.questionList[this.counter].answer;
+    this.getPosterUrl()
+
+    // Reset the form
+    const form = document.getElementById('form') as HTMLFormElement;
+    form.reset();
+    this.showNext = false;
+  }
+
+  getPosterUrl() {
+    this.Msub = this.triviaService.searchMovies(this.questionList[this.counter].movieTitle).subscribe(
+      (res) => {
+        console.log(res);
+        this.picUrl = "https://image.tmdb.org/t/p/w500" + res.results[0].poster_path;
+        console.log(this.picUrl);
+      }
+    );
   }
 
   submitAnswer(FormObj: NgForm) {
+    this.showNext = true;
     console.log(FormObj.value.answer, this.correctAnswer);
     if (FormObj.value.answer == this.correctAnswer) {
       this.questionResult.answerReceived("Correct!!!");
@@ -35,21 +60,26 @@ export class QuestionComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.triviaService.getQuestions();
-    this.subs = this.triviaService.questionSub.subscribe(
+    this.Tsub = this.triviaService.questionSub.subscribe(
       (res) => {
         if (res) {
           this.questionList = res;
           this.currentQuestion = res[0].question;
           this.correctAnswer = res[0].answer;
           console.log("inside: ", this.currentQuestion, this.correctAnswer);
+          this.getPosterUrl()
         }
 
       }
     );
     console.log("outside: ", this.currentQuestion, this.correctAnswer);
+    this.Qsub = this.questionResult.userIsRight.subscribe((result) => {
+      this.result = result
+    })
   }
 
   ngOnDestroy(): void {
-    this.subs.unsubscribe();
+    this.Qsub.unsubscribe();
+    this.Tsub.unsubscribe();
   }
 }
